f2f3609c86c8d77e2f745994bad5744576524311
Krishna Rajagopal <krish.rd@gmail.com>
2018-08-15 14:58:07 -0700

    fixing changes

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 const DataHelper = require('./db/DbHelper.js');
 
+app.set('port', (process.env.PORT || 8080));
 
 app.get('/api/v1/feed', function(req, res) {
   var userId = req.param('userId');
-  var token = req.param('token') || req.headers['x-access-token'];
+  var token = req.headers.authorization;
+  console.log('token is '+token);
+  // var token = req.param('token') || req.headers['x-access-token'];
 
   //DataHelper.insertData();
 
@@ -27,6 +30,6 @@ app.get('/api/v1/feed', function(req, res) {
 });
 
 
-app.listen(8080, function() {
+app.listen(app.get('port'), function() {
   console.log('App listening on port 8080!');
 });
